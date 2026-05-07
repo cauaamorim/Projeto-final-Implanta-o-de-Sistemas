5ecb029477308c83c2361b62ed875fdecfe66b93
CAUA DE MESQUITA AMORIM <232.975907@universidadesenaicimatec.edu.br>
2026-05-07 10:08:41 -0300

    adicionando logica de cadastro de paciente e login bem como a home de paciente

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ const { createClient } = require("@supabase/supabase-js");
 const express = require('express');
 const app = express();
 const path = require('path');
+
 const cors = require('cors');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3005;
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
@@ -19,31 +20,32 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 console.log("✅ Conexão com Supabase configurada!");
 
-
+app.use(cors());
+app.use(express.json());
 
 //rota front end 
-
 const pastaFrontend = path.join(__dirname, "../frontend");
 
-app.use(express.static(pastaFrontend, { index: false }));
+app.use(express.static(pastaFrontend));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(pastaFrontend, "login.html"));
 });
 
 
-
+//CADASTRO DE PACIENTE
 app.post('/api/cadastro', async (req, res) => {
     // Captura os dados que vieram do fetch
-    const { email, senha, celular } = req.body;
+    const {nome, email, senha, celular } = req.body;
 
     // Insere os dados na tabela do Supabase
     const { error } = await supabase 
-        .from('PacienteCadastro')
-        .insert([{ 
+        .from('pacientecadastro')
+        .insert([{  
             email: email, 
+            nome: nome,
             senha: senha, 
-            celular: celular // Adicionei o celular aqui para ele ser salvo!
+            celular: celular 
         }]);
 
     // Se o Supabase reclamar de algo, devolve o erro
@@ -51,9 +53,58 @@ app.post('/api/cadastro', async (req, res) => {
         return res.status(400).json({ erro: "Erro ao cadastrar: " + error.message });
     }
 
-    // Se deu tudo certo, devolve sucesso
-    return res.status(201).json({ mensagem: "Usuário cadastrado com sucesso!" });
+
+    return res.status(201).json({
+        mensagem: 'Cadastro realizado com sucesso'
+    });
 });
 
 
-app.listen(port, () => console.log('Servidor rodando na porta  ' + port));
+app.post('/api/login', async (req, res) => {
+    // Captura email e senha enviados pelo frontend
+    const { email, senha } = req.body;
+
+    // Procura usuário no Supabase
+    const { data, error } = await supabase
+        .from('pacientecadastro')
+        .select('*')
+        .eq('email', email)
+        .eq('senha', senha)
+        .single();
+
+    // Se der erro ou não encontrar usuário
+    if (error || !data) {
+        return res.status(401).json({
+            erro: 'Email ou senha inválidos'
+        });
+    }
+
+    // Login realizado com sucesso
+    res.status(200).json({
+        mensagem: 'Login realizado com sucesso',
+        usuario: {
+            id: data.id,
+            email: data.email,
+            celular: data.celular
+        }
+    });
+});
+
+
+app.get('/api/pacientes', async (req, res) => {
+    // Equivalente a: SELECT * FROM pacientes
+    const { data, error } = await supabase
+        .from('pacientecadastro') 
+        .select('*');
+
+    if (error) {
+        return res.status(400).json({ erro: error.message });
+    }
+
+    res.status(200).json(data);
+});
+
+
+
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
